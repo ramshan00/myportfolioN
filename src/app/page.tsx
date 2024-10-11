6803b1d6c94f058594356c7208b89735c64bfc6a
Ramsha Noshad <148268796+ramshan00@users.noreply.github.com>
2024-10-11 22:34:02 +0500

    nestapp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
   return (
     <main
       className="parallax bg-fixed bg-center bg-cover min-h-screen "
-      style={{ backgroundImage: "url('/ani.gif')" }}
+      style={{ backgroundImage: "url('./ani.gif')" }}
     >
       {/* Navbar */}
       <nav className="flex justify-between items-center py-4 px-4 md:px-10 bg-gradient-to-r from-gray-800 to-navy-900 shadow-lg">
